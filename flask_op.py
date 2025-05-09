b7dabc2adbf72d69abbbf7961987f8854709c5d2
jinjiazhang <jinjiazhang@tencent.com>
2025-05-09 22:25:12 +0800

    set_client_size

--- flask_op.py
+++ flask_op.py
@@ -13,5 +13,13 @@ def find_window():
     hwnd = op.FindWindow(class_name, title)
     return jsonify({'hwnd': hwnd})
 
+@app.route('/set_client_size')
+def set_client_size():
+    hwnd = request.args.get('hwnd', 0)
+    width = request.args.get('width', 720)
+    hight = request.args.get('hight', 480)
+    op_ret = op.SetClientSize(hwnd, width, hight)
+    return jsonify({'op_ret': op_ret})
+
 if __name__ == '__main__':
     app.run(debug=True)
--- scratch_op.js
+++ scratch_op.js
@@ -10,6 +10,11 @@ class ScratchOpBlocks {
             name: 'OP',
             blocks: [
                 {
+                    opcode: 'getHwnd',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'hwnd'
+                },
+                {
                     opcode: 'findWindow',
                     blockType: Scratch.BlockType.COMMAND,
                     text: 'find window with class [CLASS_NAME] and title [TITLE]',
@@ -25,12 +30,30 @@ class ScratchOpBlocks {
                     }
                 },
                 {
-                    opcode: 'getHwnd',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'hwnd'
+                    opcode: 'setClientSize',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'set client size of window [HWND] to width [WIDTH] and height [HEIGHT]',
+                    arguments: {
+                        HWND: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        WIDTH: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 720
+                        },
+                        HEIGHT: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 480
+                        }
+                    }
                 }
             ]
         };
+    }
+    
+    getHwnd() {
+        return this.hwnd || '';
     }
 
     findWindow(args) {
@@ -50,8 +73,22 @@ class ScratchOpBlocks {
             });
     }
 
-    getHwnd() {
-        return this.hwnd || '';
+    setClientSize(args) {
+        const params = new URLSearchParams({
+            hwnd: args.HWND,
+            width: args.WIDTH,
+            hight: args.HEIGHT
+        });
+        const url = `${this.host}/set_client_size?${params.toString()}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Client size set:', data.op_ret);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
 }
 
